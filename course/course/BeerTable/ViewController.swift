48fae228144a22175eb892efde172c7a1ce6456d
SolarHaddock46 <vladimir@matsnev.ru>
2023-10-30 19:23:41 +0300

    created BeerTableManager

--- course/course/BeerTable/ViewController.swift
+++ course/course/BeerTable/ViewController.swift
@@ -8,23 +8,6 @@
 import UIKit
 
 final class ViewController: UIViewController, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return beerData.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let beer = beerData[indexPath.row]
-        var cell = UITableViewCell()
-        var configuration = cell.defaultContentConfiguration()
-//        configuration.image = UIImage(
-        configuration.text = beer.name
-        configuration.secondaryText = beer.tagline
-        cell.contentConfiguration = configuration
-        return cell
-    }
-    
-    
-    
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -32,8 +15,6 @@ final class ViewController: UIViewController, UITableViewDataSource {
         tableView.dataSource = self
         return tableView
     }()
-
-    private var beerData: [BeerDTO] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
