25a3571283841d507cc8e6a35cee34bd3f539879
SolarHaddock46 <vladimir@matsnev.ru>
2023-10-30 19:45:26 +0300

    added beertableview

---
+++ course/course/BeerTable/BeerTableView.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+final class BeerTableView: UIView {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = .white
+        addSubviews()
+        makeConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+private extension BeerTableView {
+    func addSubviews() {
+        [tableView].forEach {
+            self.addSubview($0)
+        }
+    }
+    func makeConstraints() {
+        [tableView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+}
